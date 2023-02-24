4b6451f2149253343f3c5c49300063f9f3bacdb7
Soumya Shekar <soan9s9@gmail.com>
2023-02-24 20:39:31 +1100

    soumya raul

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import InformationCapture from "./components/InformationCapture";
 import NavBar from "./components/NavBar";
 
 function App() {
-  return <div>Feature 3 1st Coomit </div>;
+  return <div>soumya raul</div>;
 }
 
 export default App;
